4ea24ccddc8b956f77ad5f2a3b56894b877b8ff5
FrankFangRui <1605376890@qq.com>
2023-02-27 10:35:14 +0800

    submit lab1 fitstly(test)

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -32,9 +32,9 @@ public class LeapYear {
     public static boolean isLeapYear(int year){
         if(year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)){
             return true;
-        }else {
+        }else
             return false;
-        }
+        
     }
 }
 
